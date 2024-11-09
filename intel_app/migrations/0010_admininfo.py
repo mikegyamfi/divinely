3c5521395b881b5e6256925b916ba2934496f3ad
mikegyamfi <codeage20@gmail.com>
2024-11-09 17:55:01 +0000

    wallet change

--- intel_app/migrations/0010_admininfo.py
+++ intel_app/migrations/0010_admininfo.py
@@ -17,17 +17,13 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(blank=True, max_length=250, null=True)),
                 ('phone_number', models.BigIntegerField(blank=True, null=True)),
                 ('email', models.EmailField(blank=True, max_length=254, null=True)),
+                ('ishare_source', models.CharField(blank=True, choices=[('Gyasi', 'Gyasi'), ('Value4Moni', 'Value4Moni')],
+                                   default='Value4Moni', max_length=250, null=True),),
             ],
         ),
         migrations.AddField(
             model_name='admininfo',
             name='mtn_api_status',
             field=models.BooleanField(default=True),
-        ),
-        migrations.AddField(
-            model_name='admininfo',
-            name='ishare_source',
-            field=models.CharField(blank=True, choices=[('Gyasi', 'Gyasi'), ('Value4Moni', 'Value4Moni')],
-                                   default='Value4Moni', max_length=250, null=True),
         ),
     ]
